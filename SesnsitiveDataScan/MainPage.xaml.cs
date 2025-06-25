b30d800cee11466d77cb47ceb964802e9a642870
Yash Saini <31597693+yash-saini@users.noreply.github.com>
2025-06-24 20:06:38 -0400

    Busy indicator and type added.

--- SesnsitiveDataScan/MainPage.xaml.cs
+++ SesnsitiveDataScan/MainPage.xaml.cs
@@ -4,8 +4,6 @@ namespace SesnsitiveDataScan
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage(MainViewModel vm)
         {
             InitializeComponent();
--- SesnsitiveDataScan/Models/DetectedItem.cs
+++ SesnsitiveDataScan/Models/DetectedItem.cs
@@ -4,5 +4,6 @@
     {
         public string Original { get; set; }
         public string Masked { get; set; }
+        public string Type { get; set; }
     }
 }
