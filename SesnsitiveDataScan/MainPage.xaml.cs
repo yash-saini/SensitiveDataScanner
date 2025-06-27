87e61b4a81133a2218408532f2719fbd2f3b6116
Yash Saini <31597693+yash-saini@users.noreply.github.com>
2025-06-26 20:10:20 -0400

    Styles added.

--- SesnsitiveDataScan/MainPage.xaml.cs
+++ SesnsitiveDataScan/MainPage.xaml.cs
@@ -9,5 +9,14 @@ namespace SesnsitiveDataScan
             InitializeComponent();
             BindingContext = vm;
         }
+
+        private async void OnImageTapped(object sender, EventArgs e)
+        {
+            if (sender is VisualElement element)
+            {
+                await element.ScaleTo(1.2, 100);
+                await element.ScaleTo(1.0, 100);
+            }
+        }
     }
 }
